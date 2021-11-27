29653d5bd7323242e551b18f83c9b66b8c9271a2
Shivam Pandey <81798679+YodaSP@users.noreply.github.com>
2021-11-27 13:13:50 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -1,3 +1,15 @@
 def add(a,b){
   echo "Addition = ${a+b} "
 }
+
+def multiply(a,b){
+  echo "Multiplication = ${a*b} "
+}
+
+def subtract(a,b){
+  echo "Subtraction = ${a-b} "
+}
+
+def divide(a,b){
+  echo "Division = ${a/b} "
+}
